8bb3cf69519363378aa3a5e04c851d7ea55c2726
Michaël Bourjac <michael.bourjac@gmail.com>
2025-04-28 19:50:40 +0200

    feat(dashboard): redirect unauthenticated user

--- src/app/(protected)/(dashboard)/page.tsx
+++ src/app/(protected)/(dashboard)/page.tsx
@@ -1,4 +1,14 @@
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { auth } from '../../../lib/auth';
+
 const DashboardPage = async () => {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (!session) {
+    redirect('./log-in');
+  }
+
   return <div>Dashboard</div>;
 };
 
